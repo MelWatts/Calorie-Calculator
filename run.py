f89f1fbb0c9f1995beebcbf76010d11d4f63052e
Mel Watts <mel.watts@therooflightcompany.co.uk>
2021-08-12 15:52:49 +0000

    Add function for info inputs

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-# python code goes here
+# pip install py_edamam from https://www.youtube.com/watch?v=OWxvX4m_h7c
+from py_edamam import PyEdamam
+from Keys import edamam_id, edamam_key
+
+e = PyEdamam(nutrition_appid=edamam_id), nutrition_appkey=edamam_key)
+
+def bmr_calculation():
+    #get Input
+    weightInLbs - int(input("Please enter your weight in lbs: "))
+    heightInInches = int(input("Please enter your height in inces: "))
