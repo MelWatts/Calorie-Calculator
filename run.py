760c73c7e05a88fcfe2aa6b48f30a5bc5474c15e
Mel Watts <mel.watts@therooflightcompany.co.uk>
2021-08-15 10:47:45 +0000

    Redo data info, create a function for user to input data needed to calculate a female calorie defecit

--- run.py
+++ run.py
@@ -1,10 +1,29 @@
-# pip install py_edamam from https://www.youtube.com/watch?v=OWxvX4m_h7c
-from py_edamam import PyEdamam
-from Keys import edamam_id, edamam_key
+import gspread
+from google.oauth2.service_account import Credentials
 
-e = PyEdamam(nutrition_appid=edamam_id), nutrition_appkey=edamam_key)
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-def bmr_calculation():
-    #get Input
-    weightInLbs - int(input("Please enter your weight in lbs: "))
-    heightInInches = int(input("Please enter your height in inces: "))
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Calorie-Calculator')
+
+
+def get_female_data():
+    """
+    get all data filled in by user
+    """
+    data_str = input("Please enter your height in cm ")
+    print(f"Your height is  {data_str}cm")
+
+    data_str = input("Please enter your weight in lb ")
+    print(f"Your weight is  {data_str}lb")
+
+    data_str = input("Please enter your Age ")
+    print(f"Your Age is  {data_str}")
+
+get_female_data()
